11eff6e02da8b0d276e61bcc3453eab13fc6614f
prettycrunchyguy <137006173+prettycrunchyguy@users.noreply.github.com>
2024-02-28 20:45:09 -0600

    Create rokuA.user.js

---
+++ rokuA.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Roku Remote Extension A
+// @namespace    http://tampermonkey.net/
+// @version      4.0
+// @description  Redirects rokuremote website to about:blank
+// @author       crunchyguy
+// @match        http://rokuremote.glitch.me
+// @match        https://rokuremote.glitch.me
+// @match        https://prettycrunchyguy.github.io/rokuremote
+// @updateURL    https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuA.user.js
+// @downloadURL  https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuA.user.js
+// @grant        none
+// ==/UserScript==
