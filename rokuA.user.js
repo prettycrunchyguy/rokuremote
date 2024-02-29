79f1ee036e608e821a4bc524030d745d9ee61613
prettycrunchyguy <137006173+prettycrunchyguy@users.noreply.github.com>
2024-02-28 21:05:50 -0600

    Update rokuA.user.js

--- rokuA.user.js
+++ rokuA.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Roku Remote Extension A
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      1.0
 // @description  Redirects rokuremote website to about:blank
 // @author       crunchyguy
 // @match        http://rokuremote.glitch.me
 // @match        https://rokuremote.glitch.me
-// @match        https://prettycrunchyguy.github.io/rokuremote
 // @updateURL    https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuA.user.js
 // @downloadURL  https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuA.user.js
 // @grant        none
 // ==/UserScript==
+(function(){
+  window.location = 'about:blank?crunchyguy=roku'
+})();
