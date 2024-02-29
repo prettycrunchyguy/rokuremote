fae7b44550545e48d3e6d410ccf7c3a7ba05f783
prettycrunchyguy <137006173+prettycrunchyguy@users.noreply.github.com>
2024-02-28 20:56:30 -0600

    Create rokuB.user.js

---
+++ rokuB.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Roku Remote Extension B
+// @namespace    http://tampermonkey.net/
+// @version      4.0
+// @description  Helper extension for launching Roku Remote app
+// @author       crunchyguy
+// @match        about:blank
+// @updateURL    https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuB.user.js
+// @downloadURL  https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuB.user.js
+// @grant        none
+// ==/UserScript==
