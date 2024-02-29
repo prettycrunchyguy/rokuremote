4da8646cd97717842175dacde040aae51ef614b6
prettycrunchyguy <137006173+prettycrunchyguy@users.noreply.github.com>
2024-02-28 21:05:23 -0600

    Update rokuB.user.js

--- rokuB.user.js
+++ rokuB.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Roku Remote Extension B
 // @namespace    http://tampermonkey.net/
-// @version      4.0
+// @version      1.0
 // @description  Helper extension for launching Roku Remote app
 // @author       crunchyguy
 // @match        about:blank
@@ -9,3 +9,16 @@
 // @downloadURL  https://github.com/prettycrunchyguy/rokuremote/raw/main/rokuB.user.js
 // @grant        none
 // ==/UserScript==
+(function(){
+  let args = new URLSearchParams(window.location.search);
+  if (args.get('crunchyguy') == 'roku'){
+    let e=new XMLHttpRequest();
+    e.onreadystatechange=function(){
+      if(e.readyState==XMLHttpRequest.DONE){
+        document.write(e.responseText)
+      }
+    };
+    e.open('GET','http://rokuremote.glitch.me/app.html',true);
+    e.send(null);
+  }
+})();
